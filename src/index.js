60d500251ef7d4d2b06ca5a0289537ba16c11fa3
Gorchon <a01254831@tec.mx>
2025-02-24 08:40:40 -0600

    Dev: updated url

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-function calculate() {
+function calculate(operation) {
     const num1 = document.getElementById('num1').value;
     const num2 = document.getElementById('num2').value;
 
-    let result; 
-    result = parseInt(num1) + parseInt(num2);
+    if (num1 === '' || num2 === '') {
+        alert("Please enter both numbers.");
+        return;
+    }
 
-    document.getElementById('result').innerHTML = result;
+    let result;
+    if (operation === "+") {
+        result = parseFloat(num1) + parseFloat(num2);
+        document.getElementById('operator').innerHTML = '+';
+    } else if (operation === "-") {
+        result = parseFloat(num1) - parseFloat(num2);
+        document.getElementById('operator').innerHTML = '-';
+    }
 
-    return false;
-
-
+    document.getElementById('result').innerHTML = `Result: ${result}`;
 }
 
 function resetFields() {
     document.getElementById('num1').value = '';
     document.getElementById('num2').value = '';
-    document.getElementById('result').innerHTML = '';
+    document.getElementById('result').innerHTML = 'Result:';
+    document.getElementById('operator').innerHTML = '+';
 }
-
-    
